b84186ad010fd797392f2d38040da8ed030fd700
Victor G <victorg590@gmail.com>
2020-04-19 14:57:50 +0200

    Add paths for API

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,28 @@
 import express from "express";
 
+import { endpoint } from "./helpers";
+
 let app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send("Hello world"));
+app.get("/wb/:dirCid/cover", endpoint(async (req, res) => {
+    throw "Not implemented";
+}))
+
+app.get("/wb/:dirCid/dest", endpoint(async (req, res) => {
+    throw "Not implemented";
+}));
+
+app.route("/wb/:dirCid")
+    .get(endpoint(async (req, res) => {
+        throw "Not implemented";
+    }))
+    .post(endpoint(async (req, res) => {
+        throw "Not implemented";
+    }));
+
+app.get("/:cid", endpoint(async (req, res) => {
+    throw "Not implemented";
+}));
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
